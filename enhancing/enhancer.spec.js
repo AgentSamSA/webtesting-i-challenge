ca5b35fec2ed2d507f7d2f2a9421bcab79812647
Samuel Lee <samuelxlee@gmail.com>
2021-01-27 15:23:10 -0800

    wrote initial sanity test and tests for existing for an item object, as well as fleshing out the get function from enhancer

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,22 @@
 const enhancer = require('./enhancer.js');
-// test away!
+
+test("sanity", () => {
+    expect(1 + 2).toBe(3);
+    expect(3).not.toBe(4);
+    expect(1).toBeLessThan(2);
+});
+
+describe("Item class", () => {
+    let item = {};
+    beforeEach(() => {
+        item = {
+            name: "Iron Sword",
+            enhancement: 0,
+            durability: 100
+        };
+    });
+
+    it("exists", () => {
+        expect(item).toBeDefined();
+    });
+});
